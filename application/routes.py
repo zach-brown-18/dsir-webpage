139e1a52b8dfc6b8ceb0e37bb0035bf00ac11666
mhwinder <mhwinder@gmail.com>
2020-12-20 14:23:16 -0500

    Made tags clickable and resources filter based on mask.

--- application/routes.py
+++ application/routes.py
@@ -2,6 +2,10 @@ from application import app, db
 from planning import read_data
 from flask import render_template, url_for, redirect, request
 
+# Here is a global variable!
+mask_list = []
+
+
 
 @app.route("/", methods = ["GET"])
 def home():
@@ -15,14 +19,26 @@ def call():
 
 @app.route("/template_practice")
 def practice():
+    if request.args['clicked'] != '':
+        if request.args['clicked'] in mask_list:
+            mask_list.remove(request.args['clicked'])
+        else:
+            mask_list.append(request.args['clicked'])
 
     my_tags = ''
     for t in read_data.df['MAIN PAGE BRANCHES'].unique():
-        my_tags += f'<div class="tagDiv">{t}</div>\n'
+        if t in mask_list:
+            path = f"parent.location='/template_practice?clicked={t}'"
+            my_tags += f'<input class="tagMask" type=button onClick="{path}" value="{t}">\n'
+        else:
+            path = f"parent.location='/template_practice?clicked={t}'"
+            my_tags += f'<input class="tagShow" type=button onClick="{path}" value="{t}">\n'
         
     my_resources = ''
-    for r in read_data.df['PAGE DESCRIPTION']:
-        my_resources += f'<div class="resDiv">{r}</div>\n'
+    for _, row in read_data.df.iterrows():
+        if row['MAIN PAGE BRANCHES'] not in mask_list:
+            page_desc = row["PAGE DESCRIPTION"]
+            my_resources += f'<div class="resDiv">{page_desc}</div>\n'
     
     return render_template("resource_template.html", tags=my_tags, resources=my_resources)
 
